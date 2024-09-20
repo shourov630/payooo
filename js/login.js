9ee22e418075717815085ec176a0b5e505884208
shourov630 <shourovhassan6305@gmail.com>
2024-09-20 15:14:01 +0600

    ok

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ document.getElementById('login-btn').addEventListener('click',function(event){
     event.preventDefault();
 const pinNumber = document.getElementById('pin').value;
 if(pinNumber === '8521' ){
-    window.location.href = '/home.html';
+    window.location.href = './home.html';
     console.log('plase add me');
 }
 else{
