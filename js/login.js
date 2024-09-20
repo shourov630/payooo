57cdb4fbe3087a1c17ab2c2645f300813f35702f
shourov630 <shourovhassan6305@gmail.com>
2024-09-20 15:07:09 +0600

    payooo

---
+++ js/login.js
@@ -0,0 +1,12 @@
+
+document.getElementById('login-btn').addEventListener('click',function(event){
+    event.preventDefault();
+const pinNumber = document.getElementById('pin').value;
+if(pinNumber === '8521' ){
+    window.location.href = '/home.html';
+    console.log('plase add me');
+}
+else{
+    alert('vag bokachuda');
+}
+})
